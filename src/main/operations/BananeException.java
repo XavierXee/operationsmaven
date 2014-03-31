fa69f564f265f4b0d3815967013564018fafc780
Xavier de Frutos <xavier.defrutos@gmail.com>
2014-03-31 18:48:40 +0200

    arbo

---
+++ src/main/operations/BananeException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operations;
+
+/**
+ *
+ * @author xavierdefrutos
+ */
+public class BananeException extends Exception {
+
+    /**
+     * Creates a new instance of
+     * <code>BananeException</code> without detail message.
+     */
+    public BananeException() {
+    }
+
+    /**
+     * Constructs an instance of
+     * <code>BananeException</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public BananeException(String msg) {
+        super(msg);
+    }
+}
